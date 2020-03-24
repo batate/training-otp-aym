d152a3145012d557c8af778b3bdb2cd591945e4b
Nate Craver <natecraver@NCraverLaptop.local>
2020-03-24 11:58:42 -0400

    Score added

---
+++ nate/mastermind/lib/core/score.ex
@@ -0,0 +1,31 @@
+defmodule Mastermind.Score do
+  defstruct ~w[reds whites]a  # array of atom words
+
+  def new(guess, answer) do
+    __struct__(
+      reds: reds(guess, answer),
+      whites: whites(guess, answer)
+      )
+  end
+
+  defp reds(guess, answer) do
+    guess
+    |> Enum.zip(answer)
+    |> Enum.filter(fn {a, b} -> a == b end)
+    |> length
+  end
+
+  defp misses(guess, answer) do
+    (guess -- answer)
+    |> length
+  end
+
+  defp width(answer) do
+    length(answer)
+  end
+
+  defp whites(guess, answer) do
+    width(answer) - reds(guess, answer) - misses(guess, answer)
+  end
+
+end
