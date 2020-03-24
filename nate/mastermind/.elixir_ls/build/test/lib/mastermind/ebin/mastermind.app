098fca19dd035f9efee87ef28dd17c32f852587e
Nate Craver <natecraver@NCraverLaptop.local>
2020-03-24 16:23:40 -0400

    Stuff

--- nate/mastermind/.elixir_ls/build/test/lib/mastermind/ebin/mastermind.app
+++ nate/mastermind/.elixir_ls/build/test/lib/mastermind/ebin/mastermind.app
@@ -2,6 +2,6 @@
              [{applications,[kernel,stdlib,elixir,logger]},
               {description,"mastermind"},
               {modules,['Elixir.Mastermind','Elixir.Mastermind.Game',
-                        'Elixir.Mastermind.Score']},
+                        'Elixir.Mastermind.Score','Elixir.Mastermind.Server']},
               {registered,[]},
               {vsn,"0.1.0"}]}.
--- nate/mastermind/lib/mastermind.ex
+++ nate/mastermind/lib/mastermind.ex
@@ -1,18 +1,16 @@
 defmodule Mastermind do
-  @moduledoc """
-  Documentation for Mastermind.
-  """
+  alias Mastermind.Server
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Mastermind.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def start(answer) do
+    Server.start_link(answer)
   end
+
+  def move(pid, guess) do
+    Server.move(pid, guess)
+  end
+
+  def display(pid) do
+    Server.display(pid)
+  end
+
 end
